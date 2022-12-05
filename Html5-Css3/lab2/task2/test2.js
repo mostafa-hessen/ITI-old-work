57d0804e8b29ef2399961c6cfa7c01c6aa97bfc6
Your Name <you@example.com>
2022-12-05 12:56:07 +0200

    addtion rdw responsive day

---
+++ Html5-Css3/lab2/task2/test2.js
@@ -0,0 +1,11 @@
+let fillColor=document.getElementById('fillColor')
+let strokeColor=document.getElementById('strokeColor')
+let square=document.querySelector('.react')
+let circle=document.querySelector('.circl')
+let line=document.querySelector('.lineStright')
+let eraser=document.querySelector('.eraser')
+
+
+
+
+console.log(fillColor,strokeColor);
